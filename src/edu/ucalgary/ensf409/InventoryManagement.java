dd2295a31912988d0b7936a3f3b0525347605fda
MrAvatin <62223906+MrAvatin@users.noreply.github.com>
2021-03-27 13:07:31 -0600

    Some initial implementation of InventoryManagement

--- src/edu/ucalgary/ensf409/InventoryManagement.java
+++ src/edu/ucalgary/ensf409/InventoryManagement.java
@@ -1,5 +1,27 @@
 package edu.ucalgary.ensf409;
 
 public class InventoryManagement {
+    private String[][] inventory;
+
+    public InventoryManagement(int size){
+        inventory = new String[size][2];
+    }
+
+    public int getStock(String id){
+        for (String[] strings : inventory) {
+            if (strings[0].equals(id)) {
+                return Integer.parseInt(strings[1]);
+            }
+        }
+        return 0;
+    }
+
+    public void setStock(String id, int count){
+        for (String[] strings : inventory) {
+            if (strings[0].equals(id)) {
+                strings[1] = String.valueOf(count);
+            }
+        }
+    }
 
 }
