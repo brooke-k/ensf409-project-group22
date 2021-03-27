b55a71586faa3e394e8bca9d915dfa85e23b6fb6
MrAvatin <62223906+MrAvatin@users.noreply.github.com>
2021-03-27 11:43:34 -0600

    Adding connection and close for database

--- src/edu/ucalgary/ensf409/DatabaseIO.java
+++ src/edu/ucalgary/ensf409/DatabaseIO.java
@@ -5,12 +5,47 @@ public class DatabaseIO {
     public final String USERNAME;
     public final String PASSWORD;
     private Connection myConnect;
+
+    /**
+     *
+     * @param DBURL The location at which the database exits.
+     * @param USERNAME The user's username.
+     * @param PASSWORD The user's password.
+     */
+    public DatabaseIO(String DBURL, String USERNAME, String PASSWORD){
+        this.DBURL = DBURL;
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
+    }
+    public String getDburl() {
+        return DBURL;
+    }
+
+    public String getUsername() {
+        return USERNAME;
+    }
+
+    public String getPassword() {
+        return PASSWORD;
+    }
+
     /**
      * Tries to initialize the connection with the database.
      */
     public void  initializeConnection(){
         try {
             myConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    /**
+     * Closes the connection to the database.
+     */
+    public void close(){
+        try {
+            myConnect.close();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
