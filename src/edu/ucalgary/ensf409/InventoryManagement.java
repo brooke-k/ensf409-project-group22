f966d0fb7ea6c09e104a6f438143c09ea6da94ac
MrAvatin <62223906+MrAvatin@users.noreply.github.com>
2021-03-27 13:35:13 -0600

    Changed constructor, now takes in an array of itemID.

--- src/edu/ucalgary/ensf409/InventoryManagement.java
+++ src/edu/ucalgary/ensf409/InventoryManagement.java
@@ -4,6 +4,10 @@ public class InventoryManagement {
 
     private String[][] inventory;
 
+    /**
+     *  Takes in an array of itemID and creates a 2d inventory array accordingly.
+     * @param itemID Array of ID's of all items
+     */
     public InventoryManagement(String[] itemID){
         inventory = new String[itemID.length][2];
         for (int i = 0; i < itemID.length; i++) {
@@ -11,6 +15,11 @@ public class InventoryManagement {
         }
     }
 
+    /**
+     * Returns the stocks for a particular id.
+     * @param id The id of the item
+     * @return amount of stock in hand
+     */
     public int getStock(String id){
         for (String[] strings : inventory) {
             if (strings[0].equals(id)) {
@@ -20,6 +29,11 @@ public class InventoryManagement {
         return 0;
     }
 
+    /**
+     * Updates the stock for an item.
+     * @param id The selected id to change the stock of.
+     * @param count The new stock count for the item.
+     */
     public void setStock(String id, int count){
         for (String[] strings : inventory) {
             if (strings[0].equals(id)) {
@@ -28,6 +42,12 @@ public class InventoryManagement {
         }
     }
 
+    /**
+     * Checks through the array if the item exists.
+     * We shouldn't need to use this method.
+     * @param id The ID of the item which need to be checked.
+     * @return True of false, depending on if the item exists.
+     */
     public boolean itemExists(String id){
         for(String[] strings : inventory){
             if(strings[0].equals(id)){
