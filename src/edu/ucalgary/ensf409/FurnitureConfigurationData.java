05d34c25a2491caccfe6b6005b30e5e30aefbc01
Chris <78698227+cjchanx@users.noreply.github.com>
2021-03-27 15:33:11 -0600

    PriceOptimizer update

--- src/edu/ucalgary/ensf409/FurnitureConfigurationData.java
+++ src/edu/ucalgary/ensf409/FurnitureConfigurationData.java
@@ -1,7 +1,7 @@
 package edu.ucalgary.ensf409;
 
 /*
-    FurnitureConfigurationData contains two parallel arrays, one with
+    FurnitureConfigurationData contains three parallel arrays, one with
     the specified furniture ID and one with the specified parts for a
     specific furniture configuration. For instance
      id[0] = D0890, boolean[0] = {F, F, T}, price[0] = 25
