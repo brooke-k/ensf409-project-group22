c227478d5e0e718f41aa3be745b9730830fc9519
MrAvatin <62223906+MrAvatin@users.noreply.github.com>
2021-03-27 11:42:10 -0600

    DatabaseIO

--- src/edu/ucalgary/ensf409/DatabaseIO.java
+++ src/edu/ucalgary/ensf409/DatabaseIO.java
@@ -1,5 +1,18 @@
 package edu.ucalgary.ensf409;
-
+import java.sql.*;
 public class DatabaseIO {
-    
+    public final String DBURL;
+    public final String USERNAME;
+    public final String PASSWORD;
+    private Connection myConnect;
+    /**
+     * Tries to initialize the connection with the database.
+     */
+    public void  initializeConnection(){
+        try {
+            myConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
