e42ef9c9e07fe46ea541abf40b270ca8e9865d28
Brooke <brooke.kindleman@gmail.com>
2021-03-27 21:51:43 -0600

    Added method for getting user input for user request.
    Allows for correct inputs to be used.

--- src/edu/ucalgary/ensf409/Main.java
+++ src/edu/ucalgary/ensf409/Main.java
@@ -7,15 +7,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         UserIO input = new UserIO();
-        // Initiate a menu object
+
         int selection = input.menu();
+        input.processInput(selection);
 
 
-        System.out.println("You entered: " + selection);
-        // Example taking in input
-        System.out.print("Please enter user input: ");
-        int in = input.readIntUntilAccepted(0, 4);
-        System.out.println("You entered: " + in);
+       //System.out.println("You entered: " + selection);
+      // System.out.print("Please enter user input: ");
+        //int in = input.readIntUntilAccepted(0, 4);
+        //System.out.println("You entered: " + in);
 
         // Testing PriceOptimizer
 
