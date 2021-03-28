82999d9e6deec5bceff6388da904ed73a19a0f6f
MrAvatin <62223906+MrAvatin@users.noreply.github.com>
2021-03-27 18:48:24 -0600

    added price optimizer

--- src/edu/ucalgary/ensf409/PriceOptimizer.java
+++ src/edu/ucalgary/ensf409/PriceOptimizer.java
@@ -9,6 +9,7 @@ public class PriceOptimizer {
     private boolean[][] parts;
     private String[] id;
     private int[] price;
+    private int min = 0;
 
     /**
      * PriceOptimizer will construct
@@ -29,8 +30,51 @@ public class PriceOptimizer {
      * current FurnitureConfigurationData.
      */
     public String[] optimize() {
-        String[] ids = new String[10];
+        min = 0;
+        for(int p : price){
+            min += p;
+        }
+        int[] indexs = new int[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+            indexs[i] = i;
+        }
+        for (int i = 1; i < partCount; i++) {
+            combination(indexs, i);
+        }
+
+        String[] ids = new String[partCount];
         return ids;
+    }
+
+    private void recursion(int[] array, int[] data, int start, int end, int index, int r){
+
+        if(index == r){
+            if(compatible(data)){
+                //WOO
+            }
+        }
+
+        for (int i = start; i <= end && (end + 1 - i) >= (r - index); i++) {
+            data[index] = array[i];
+            recursion(array, data, i+1, end, index+1, r);
+        }
+
+    }
+
+    private void combination(int[] array, int comb){
+        int[] data = new int[comb];
+        recursion(array, data, 0, data.length-1, 0, comb);
+    }
+
+
+    /**
+     * @param index
+     * @return
+     */
+    private boolean compatible(int[] index){
+        boolean[] comp = new boolean[partCount];
+        //do later
+        return false;
     }
 
     /**
