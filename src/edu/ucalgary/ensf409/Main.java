4ffb05c45a1f1127b17205bf010748abc9f6e2c9
Brooke <brooke.kindleman@gmail.com>
2021-04-02 12:28:06 -0600

    Documentation for main and program
    Added documentation for the main class and main method, as well as documentation with all of our names in the Main class file.

--- src/edu/ucalgary/ensf409/Main.java
+++ src/edu/ucalgary/ensf409/Main.java
@@ -1,7 +1,39 @@
+/**
+ * ENSF 409 Winter 2021 Final Group Project
+ * Program designed with the intention of allowing a sustainable
+ * supply chain furniture company to provide methods of purchasing
+ * reconstructed or refurbished furniture items from their catalog, at the
+ * lowest price possible for each item.
+ *
+ * @author <a href="christopher.chan2@ucalgary.ca>Christopher Chan</a>
+ * @author <a href="amnah.hussain@ucalgary.ca>Amnah Hussain</a>
+ * @author <a href="brooke.kindleman@ucalgary.ca>Brooke Kindleman</a>
+ * @author <a href="neeraj.sunikumar@ucalgary.ca>Neeraj Sunil Kumar</a>
+ *
+ * @since 1.0
+ *
+ * @version 3.2
+ */
+
 package edu.ucalgary.ensf409;
 
+
+/**
+ * Class Main manages the start up and shut down of the program,
+ * and contains the main method responsible for running the program.
+ */
 public class Main {
 
+    /**
+     * Main method for the program. Uses an infinite loop for calling
+     * the UserIO input menu method, which provides interaction with
+     * the user.
+     * Infinite loop will be stopped if the user makes the appropriate
+     * selection in the menu, where the program will end sucessfully.
+     *
+     * @param args Unused inputs from the command line when program
+     *             is executed.
+     */
     public static void main(String[] args) {
         UserIO input = new UserIO();
         int selection = input.menu();
