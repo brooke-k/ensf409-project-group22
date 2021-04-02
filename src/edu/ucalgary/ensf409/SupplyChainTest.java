fb27146500c84ebaf3f468d1faf5e637925d3dd1
Brooke <brooke.kindleman@gmail.com>
2021-04-02 12:44:52 -0600

    Added the class file Test File
    Added the test file where all the tests for the program will be stored and run out of.
    Added documentation at the top of the file with group member names and version numbers.

---
+++ src/edu/ucalgary/ensf409/SupplyChainTest.java
@@ -0,0 +1,24 @@
+/**
+ * ENSF 409 Winter 2021 Final Group Project
+ * SupplyChainTest is a program designed to test the methods and construction
+ * of all files in the Supply Chain project using Junit 4.13.2
+ *
+ * @author <a href="christopher.chan2@ucalgary.ca>Christopher Chan</a>
+ * @author <a href="amnah.hussain@ucalgary.ca>Amnah Hussain</a>
+ * @author <a href="brooke.kindleman@ucalgary.ca>Brooke Kindleman</a>
+ * @author <a href="neeraj.sunikumar@ucalgary.ca>Neeraj Sunil Kumar</a>
+ *
+ * @since 1.0
+ *
+ * @version 1.0
+ *
+ *
+ */
+
+package edu.ucalgary.ensf409;
+import org.junit.*;
+
+
+public class SupplyChainTest {
+
+}
