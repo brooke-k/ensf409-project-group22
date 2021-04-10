b161c494c4c2866f2aaf142f86aa2cc6b3e257aa
Brooke <brooke.kindleman@gmail.com>
2021-04-10 10:50:39 -0600

    Fixed lack of quotation marks in documentation email addresses

--- src/edu/ucalgary/ensf409/SupplyChainTest.java
+++ src/edu/ucalgary/ensf409/SupplyChainTest.java
@@ -3,10 +3,10 @@
  * SupplyChainTest is a program designed to test the methods and construction
  * of all files in the Supply Chain project using Junit 4.13.2
  *
- * @author <a href="christopher.chan2@ucalgary.ca>Christopher Chan</a>
- * @author <a href="amnah.hussain@ucalgary.ca>Amnah Hussain</a>
- * @author <a href="brooke.kindleman@ucalgary.ca>Brooke Kindleman</a>
- * @author <a href="neeraj.sunikumar@ucalgary.ca>Neeraj Sunil Kumar</a>
+ * @author <a href="christopher.chan2@ucalgary.ca">Christopher Chan</a>
+ * @author <a href="amnah.hussain@ucalgary.ca">Amnah Hussain</a>
+ * @author <a href="brooke.kindleman@ucalgary.ca">Brooke Kindleman</a>
+ * @author <a href="neeraj.sunikumar@ucalgary.ca">Neeraj Sunil Kumar</a>
  *
  * @since 1.0
  *
